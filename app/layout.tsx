3a6f8db91a00a293c28c4ee20cf9e6bd00e66ecb
Lucas Cisternas <l.cisternasopazo@gmail.com>
2024-04-14 19:12:57 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({  weight: ["100", "200", "300", "400", "500", "600", "700"], subsets: ["latin"] });
+const poppins = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "IUMP TALCA",
   description: "Iglesia unida metodista pentecostal primera de talca",
-};
+  icons: {
+    icon: '/logo.png', // /public path
+  },
+}
 
 export default function RootLayout({
   children,
